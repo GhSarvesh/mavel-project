f171edcbe9263da190bb268058998e0b6c795792
Ubuntu <ubuntu@ip-172-31-1-245.ap-south-1.compute.internal>
2025-05-03 14:02:48 +0000

    maven-commit1

--- src/main/com/fortune/myapp/App.java
+++ src/main/com/fortune/myapp/App.java
@@ -5,7 +5,7 @@ package com.fortune.myapp;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!, Updated Program";
+    private static final String MESSAGE = "Hello World!, Updated Program and Testing";
 
     public App() {}
 
